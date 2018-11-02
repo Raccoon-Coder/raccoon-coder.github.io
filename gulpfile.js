eadbfc56045d57fd7f95a0c21226c032be055886
Raccoon-Coder <joona.matias.aalto@gmail.com>
2018-11-02 08:20:41 +0200

    Add build step at start of gulp:watch task

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,19 @@ const nunjucksRender = require('gulp-nunjucks-render');
 const htmlmin = require('gulp-html-minifier');
 
 gulp.task('build', function () {
+  return buildNjk()
+});
+gulp.task('build:watch', function () {
+  buildNjk();
+  gulp.watch(['templates/**/*.njk'], gulp.series('build'));
+});
+
+function buildNjk() {
   return gulp.src('templates/pages/**/*.+(html|njk)')
     .pipe(nunjucksRender({
       path: ['templates/partials'],
       ext: '.html'
     }))
     .pipe(htmlmin({ collapseWhitespace: true }))
-    .pipe(gulp.dest('./'))
-});
-gulp.task('build:watch', function () {
-  gulp.watch(['templates/**/*.njk'], gulp.series('build'));
-});
+    .pipe(gulp.dest('./'));
+}
